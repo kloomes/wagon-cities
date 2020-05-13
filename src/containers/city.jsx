947545a24b6824b69acd9cf24ea46780f32b8f86
Karl Loomes <kloomes@hotmail.com>
2020-05-13 11:57:18 +0100

    Basic react components added

---
+++ src/containers/city.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const City = () => {
+  return (
+    <div>London</div>
+  );
+};
+
+export default City;
---
+++ src/containers/city_list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import City from '../containers/city';
+
+const CityList = () => {
+  return (
+    <div>
+      <City />
+    </div>
+  );
+};
+
+export default CityList;
