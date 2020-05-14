6853289c461d897abd4864e1dab7a690f0ea9309
Karl Loomes <kloomes@hotmail.com>
2020-05-14 16:41:41 +0100

    Actve city formatting and img added

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const ActiveCity = (props) => {
-  console.log(props.activeCity);
-
   if (!props.activeCity) {
     return (
       <div className="active-city">
@@ -12,9 +10,13 @@ const ActiveCity = (props) => {
     );
   }
 
+  const url = `https://kitt.lewagon.com/placeholder/cities/${props.activeCity.slug}`;
+
   return (
     <div className="active-city">
-      {props.activeCity.name}
+      <h3>{props.activeCity.name}</h3>
+      <p>{props.activeCity.address}</p>
+      <img src={url} width="100%" alt={props.activeCity.name} />
     </div>
   );
 };
