2de52f4ee122d0b22e7ee43149d72a586a4e2d8d
Karl Loomes <kloomes@hotmail.com>
2020-05-14 12:21:13 +0100

    List action and connectect added

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@
 import cities from '../data/cities';
 
 export function setCities() {
+
+
   return {
     type: 'SET_CITIES',
     payload: cities
--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions/index';
 
 import City from '../containers/city';
 
 class CityList extends Component {
   componentWillMount() {
-
+    this.props.setCities();
   }
 
-  render() {
+  renderList() {
     return this.props.cities.map((city) => {
       return (
-        <ul className="list-group cities">
-          <City city={city} key={city.name} />
-        </ul>
+        <City city={city} key={city.name} />
       );
     });
   }
+
+  render() {
+    return (
+      <ul className="list-group cities">
+        { this.renderList() }
+      </ul>
+    );
+  }
 }
 
-export default CityList;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities: setCities },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
